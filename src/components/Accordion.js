b48be6b4cb92c379f7c72b6d3d4217258003476f
Spencer Lazzar <spencerlazzar@yahoo.com>
2021-01-08 20:37:22 -0800

    Accordion Widget Complete

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
 
-  const renderedItems = items.map(item => {
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  }
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className={`title ${active}`}
+          onClick={() => onTitleClick(index)}
+        >
           <i className="dropdown ican"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
-      </React.Fragment>
+      </React.Fragment >
     )
   })
 
